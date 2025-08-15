c3f07627b4e8a78d14c59e0feadd0a89a51e4bb0
Harshwardhan <harshvardhansonawane2@gmail.com>
2025-08-15 19:49:29 +0530

    Add restro-list module, component, service

---
+++ src/app/restro-list.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RestroListComponent } from './restro-list.component';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    RestroListComponent
+  ]
+})
+export class RestroListModule { }
